4bf7998ab3d55acfaad19e0b5005b82d23826276
yangfan <yf_rjxy@live.cn>
2017-10-04 16:41:31 +0800

    first commit

---
+++ golang/Main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	//"fmt"
+
+	"github.com/xtaci/kcp-go"
+)
+
+const port = ":5050"
+
+func ListenTest() (*kcp.Listener, error) {
+	return kcp.ListenWithOptions(port, nil, 2, 2)
+}
+
+func Server() {
+	l, err := ListenTest()
+	if err != nil {
+		panic(err)
+	}
+	l.SetDSCP(46)
+	for {
+		s, err := l.AcceptKCP()
+		if err != nil {
+			panic(err)
+		}
+
+		go HandleClient(s)
+	}
+}
+func HandleClient(conn *kcp.UDPSession) {
+	conn.SetWindowSize(1024, 1024)
+	conn.SetNoDelay(1, 20, 2, 1)
+	conn.SetStreamMode(false)
+	//fmt.Println("new client", conn.RemoteAddr())
+	buf := make([]byte, 65536)
+	count := 0
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+		count++
+		//fmt.Println("received:", string(buf[:n]))
+		conn.Write(buf[:n])
+	}
+}
+
+func main() {
+	Server()
+}
